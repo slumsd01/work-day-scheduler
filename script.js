9541b4eb8ed556ad281c8da2ac61e39bfb0dadf4
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-11-06 16:17:53 -0400

    color changes with time

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //display current date
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+let now = moment();
+$("#currentDay").text(now.format('MMMM Do YYYY, h:mm a'));
+
+let currenthour = moment().hour();
+console.log(currenthour);
+
+//display correct colors
+$(".timeblock").each(function() {
+    blocktime = $(this).attr("id");
+    console.log(blocktime , currenthour);
+
+    if (blocktime < currenthour) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (blocktime == currenthour) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }
+})
