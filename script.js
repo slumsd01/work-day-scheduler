c48ee494a7a38825373dfad22e23525bd5ad834b
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-11-07 08:58:04 -0500

    added local storage

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 //display current date
 let now = moment();
 $("#currentDay").text(now.format('MMMM Do YYYY, h:mm a'));
-
 let currenthour = moment().hour();
-console.log(currenthour);
 
-//display correct colors
-$(".timeblock").each(function() {
+$(".time-block").each(function() {
     blocktime = $(this).attr("id");
-    console.log(blocktime , currenthour);
 
     if (blocktime < currenthour) {
         $(this).removeClass("future");
@@ -26,3 +22,22 @@ $(".timeblock").each(function() {
         $(this).addClass("future");
     }
 })
+
+$(document).ready(function () {
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var hour = $(this).parent().attr("id");
+        localStorage.setItem(hour, text);
+    })
+})
+
